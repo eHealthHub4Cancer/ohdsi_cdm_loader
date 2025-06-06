f8434d8540fb3aa6bebb71c9ef8e0dd689ec6934
David Akwuru <daakwuru@gmail.com>
2025-06-07 00:43:23 +0100

    Add Docker setup with R and Python

---
+++ install_r_packages.R
@@ -0,0 +1,3 @@
+install.packages(c('devtools', 'DatabaseConnector', 'SqlRender', 'arrow'), repos='https://cloud.r-project.org')
+devtools::install_github('OHDSI/CommonDataModel')
+devtools::install_github('OHDSI/ETL-Synthea')
